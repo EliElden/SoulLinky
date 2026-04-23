35d7acb88051c09ee65172685adcae23033ff0d7
Elina Morozova <a.devirouz@gmail.com>
2026-04-23 14:36:04 +0300

    changed config

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 PROXY = os.getenv("PROXY_URL")
 
+#задокументировать следующие 2 строчки, если прокси не нужен
 if PROXY:
-    apihelper.proxy = {'https': PROXY}
+   apihelper.proxy = {'https': PROXY}
 
 bot = telebot.TeleBot(TOKEN)
